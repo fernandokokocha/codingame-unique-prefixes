301f70e97cc40658d98d605ddb0c1fb8dc365069
Bartosz Krajka <krajka.bartosz@gmail.com>
2022-09-27 12:02:36 +0200

    Test 3: 2 words 1 letter the same

--- unique_prefixes_spec.rb
+++ unique_prefixes_spec.rb
@@ -10,4 +10,9 @@ RSpec.describe UniquePrefixes do
   it "2 words 1 letter each different" do
     expect(unique_prefixes.run(["a", "b"])).to eq(["a", "b"])
   end
+
+  # * A word can be given several times in the given list. All the occurences of this word will share the same prefix.
+  it "2 words 1 letter the same" do
+    expect(unique_prefixes.run(["a", "a"])).to eq(["a", "a"])
+  end
 end
